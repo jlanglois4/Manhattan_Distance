770a8547c88a588fd400b6f68b4dfa2602a30bfe
jlanglois4 <jlanglois4@my.wctc.edu>
2022-04-12 13:57:54 -0500

    Initial commit

---
+++ src/test/java/edu/wctc/MPointTest.java
@@ -0,0 +1,22 @@
+package edu.wctc;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MPointTest {
+
+    private MPoint mPoint;
+
+    @BeforeEach
+    void setUp(){
+        mPoint = new MPoint();
+    }
+
+    @Test
+    void canStartProgram(){
+        
+    }
+
+
+
+}
