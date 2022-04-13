847cc25885ed80d4c2ea8871ba36791ff123f808
jlanglois4 <jlanglois4@my.wctc.edu>
2022-04-12 19:45:01 -0500

    Updated formula to accept doubles

--- src/test/java/edu/wctc/MPointTest.java
+++ src/test/java/edu/wctc/MPointTest.java
@@ -9,12 +9,12 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MPointTest {
 
     @BeforeEach
-    void CanCreate(){
+    void setUp(){
         MPoint Point1 = new MPoint(5,10);
         MPoint mPoint2 = new MPoint(10, 42);
     }
 
-    private int formula(int x1, int y1, int x2, int y2){
+    private double formula(double x1, double y1, double x2, double y2){
         return Math.abs(x2 - x1) + Math.abs(y2 - y1);
     }
 
